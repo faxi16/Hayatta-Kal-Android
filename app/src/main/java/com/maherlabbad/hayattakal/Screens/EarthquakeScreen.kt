e165d9f5540b325dee4d9ff6e456d55e107fd21b
Maher Labbad <labbadmaher11@gmail.com>
2025-08-07 15:52:35 +0300

    Navigation And earthquake screen finished

---
+++ app/src/main/java/com/maherlabbad/hayattakal/Screens/EarthquakeScreen.kt
@@ -0,0 +1,154 @@
+package com.maherlabbad.hayattakal.Screens
+
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.maherlabbad.hayattakal.model.EarthquakeModel
+import com.maherlabbad.hayattakal.viewmodel.EarthquakeViewModel
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Earthquake_screen(Earthquakeviewmodel : EarthquakeViewModel,navController: NavController){
+    val earthquakes by Earthquakeviewmodel.earthquakes.collectAsState()
+    val currentSource by Earthquakeviewmodel.dataSource.collectAsState()
+    val isLoading by Earthquakeviewmodel.isLoading.collectAsState()
+    val context = LocalContext.current
+
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "Deprem Takibi",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                    titleContentColor = MaterialTheme.colorScheme.onSurface
+                ),
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.navigate("MainScreen")
+                    }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Geri")
+                    }
+                },actions = {
+                    IconButton(
+                        onClick = {
+                            Earthquakeviewmodel.toggleDataSource()
+                            val nextSource = if (currentSource == "AFAD") "Kandilli" else "AFAD"
+                            Toast.makeText(context, "$nextSource verileri yükleniyor...", Toast.LENGTH_SHORT).show()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Refresh,
+                            contentDescription = "Veri Kaynağını Değiştir"
+                        )
+                    }
+                }
+            )
+        }
+    ) {innerPadding ->
+
+        if (isLoading) {
+            Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(items = earthquakes, key = { it.eventId }) { earthquake ->
+                    EarthquakeRow(Earthquake = earthquake)
+                }
+            }
+
+        }
+
+    }
+}
+
+@Composable
+fun EarthquakeRow(Earthquake: EarthquakeModel){
+
+    val Magnitude = Earthquake.magnitude.toDouble()
+    var color = Color.Black
+    if(Magnitude < 5 && Magnitude > 3.5){
+        color = Color.Green
+    }else if(Magnitude > 4.5){
+        color = Color.Red
+    }
+
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .clickable(onClick = {  }),
+        shape = RoundedCornerShape(25.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainer)) {
+        Row(modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 28.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(20.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+
+                Text(Earthquake.location)
+                Text(Earthquake.date)
+            }
+
+            Text(Earthquake.magnitude, color = color, fontWeight = FontWeight.Bold, style = MaterialTheme.typography.headlineMedium)
+
+        }
+    }
+}
+fun getStartAndEndDatesForLast24Hours(): Pair<String, String> {
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+
+    val calendar = Calendar.getInstance()
+    val endDate = dateFormat.format(calendar.time)
+
+    calendar.add(Calendar.HOUR_OF_DAY, -24)
+    val startDate = dateFormat.format(calendar.time)
+
+    return Pair(startDate, endDate)
+}
